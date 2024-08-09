e34a8cde4b5e059b92f30d0263d10b0193cc1244
Honey <honey9122836929@gmail.com>
2024-08-09 22:48:05 +0530

    Fixed MongoDB connection issue by updating connection string handling

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if(process.env.NODE_ENV!="production"){
   require('dotenv').config()
 }
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -37,8 +36,6 @@ async function main() {
 
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
@@ -54,25 +51,16 @@ const sessionOptions = {
     httpOnly: true,
   },
 };
-
-
 // app.get("/", (req, res) => {
 //   res.send("Hi, I am root");
 // });
-
-
-
  app.use(session(sessionOptions));
  app.use(flash());
-
-
  app.use(passport.initialize());
  app.use(passport.session());
  passport.use(new LocalStrategy(User.authenticate()));
-
  passport.serializeUser(User.serializeUser());
  passport.deserializeUser(User.deserializeUser());
-
  app.use((req, res, next) => {
   console.log('Current User:', req.user); // Debugging line
   res.locals.success = req.flash('success');
@@ -80,8 +68,6 @@ const sessionOptions = {
   res.locals.currUser = req.user;
   next();
 });
-
-
 // app.get("/demouser",async (req,res)=>{
 //   let fakeUser= new User({
 //     email:"honey9122836921@getMaxListeners.com",
@@ -90,21 +76,16 @@ const sessionOptions = {
 //     let registeredUser=await User.register(fakeUser,"helloworld");
 //     res.send(registeredUser)
 // })
-
 app.use("/listings", listingRouter);
 app.use("/listings/:id/review", reviewRouter);
 app.use("/",userRouter)
-
-
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
 });
-
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { message });
 });
-
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
