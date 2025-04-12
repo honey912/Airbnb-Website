90e100b1c638a84dc868f8e1cc775a3702357bc0
Honey <honey9122836929@gmail.com>
2025-04-12 15:28:43 +0530

    connected atlas

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://honey9122836929:sawXOZIWjnWgikkQ@cluster0.xdie2xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
   .then(() => {
